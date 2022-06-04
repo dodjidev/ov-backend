babe244ed90303361e812abccd7f7792860cbc38
dodjidev <dadevcontact@gmail.com>
2022-06-04 11:45:44 +0000

    edit message

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/' , (req, res)=>{
-    res.status(200).json({message: "Welcome to ov-backend service"});
+    res.status(200).json({message: "Welcome to ov-backend service. You are a champions"});
 })
 
 
