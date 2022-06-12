8f59a4db6c1a6a63e7053867fd47fea7e61bf624
dodjidev <dadevcontact@gmail.com>
2022-06-12 21:12:32 +0100

    cahnged

--- src/app.js
+++ src/app.js
@@ -4,10 +4,10 @@ const configs = require('./configs');
 const mongoose = require('mongoose');
 const productRouter = require('./routes/product.rt')
 
-mongoose.connect("mongodb+srv://dadev:Mw4NsmQlClFsnALr@jrcluster.hllujh9.mongodb.net/Products?retryWrites=true&w=majority") //configs.MONGOOSE_URI
+mongoose.connect(configs.MONGOOSE_URI) //
     .then(res => {
         console.log('successfuly connected to mongoose !!!')
-        app.listen( process.env.PORT || 8500, ()=> console.log('server started successfully !!')); //configs.SERVER_PORT
+        app.listen( process.env.PORT || configs.SERVER_PORT , ()=> console.log('server started successfully !!')); //
     }).catch(err => console.log('mongoose connection error:'+err.message))
 
 app.use(express.urlencoded({extended: true}))
