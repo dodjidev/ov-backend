f615e82a41fd8def81aa4a4cf7ab6d1eccdf1cd9
dodjidev <dadevcontact@gmail.com>
2022-06-13 07:42:19 +0100

    another commit

--- src/app.js
+++ src/app.js
@@ -5,19 +5,22 @@ const mongoose = require('mongoose');
 const serviceRoute = require('./routes/service.rt')
 const serviceTypeRoute = require('./routes/serviceType.rt')
 
-mongoose.connect(configs.MONGOOSE_URI) 
-    .then(res => {
-        console.log('successfuly connected to mongoose !!!')
-        app.listen( process.env.PORT || configs.SERVER_PORT , ()=> console.log('server started successfully !!')); //
-    }).catch(err => console.log('mongoose connection error:'+err.message))
+// mongoose.connect(configs.MONGOOSE_URI) 
+//     .then(res => {
+//         console.log('successfuly connected to mongoose !!!')
+//         app.listen( process.env.PORT || configs.SERVER_PORT , ()=> console.log('server started successfully !!')); //
+//     }).catch(err => console.log('mongoose connection error:'+err.message))
 
-app.use(express.urlencoded({extended: true}))
-app.use(express.json({extended: true}))
+// app.use(express.urlencoded({extended: true}))
+// app.use(express.json({extended: true}))
 
 app.get('/', (req, res)=>{
     res.status(200).json({message: "Well done !!!"});
 })
 
-app.use('/services', serviceRoute)
-app.use('/service-types', serviceTypeRoute)
+// app.use('/services', serviceRoute)
+// app.use('/service-types', serviceTypeRoute)
+
+
+app.listen( process.env.PORT || configs.SERVER_PORT , ()=> console.log('server started successfully !!')); //
 
