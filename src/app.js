8933150a7e7c70d10447b0e917c9b4d97b91325b
dodjidev <dadevcontact@gmail.com>
2022-06-13 08:06:13 +0100

    test 4

--- src/app.js
+++ src/app.js
@@ -5,14 +5,14 @@ const mongoose = require('mongoose');
 const serviceRoute = require('./routes/service.rt')
 // const serviceTypeRoute = require('./routes/serviceType.rt')
 
-// mongoose.connect(configs.MONGOOSE_URI) 
-//     .then(res => {
-//         console.log('successfuly connected to mongoose !!!')
-//         app.listen( process.env.PORT || configs.SERVER_PORT , ()=> console.log('server started successfully !!')); //
-//     }).catch(err => console.log('mongoose connection error:'+err.message))
+mongoose.connect(configs.MONGOOSE_URI) 
+    .then(res => {
+        console.log('successfuly connected to mongoose !!!')
+        app.listen( process.env.PORT || configs.SERVER_PORT , ()=> console.log('server started successfully !!')); //
+    }).catch(err => console.log('mongoose connection error:'+err.message))
 
-// app.use(express.urlencoded({extended: true}))
-// app.use(express.json({extended: true}))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json({extended: true}))
 
 app.get('/', (req, res)=>{
     res.status(200).json({message: "Well done !!!", port: configs.SERVER_PORT});
@@ -22,5 +22,5 @@ app.get('/', (req, res)=>{
 // app.use('/service-types', serviceTypeRoute)
 
 
-app.listen( process.env.PORT || configs.SERVER_PORT , ()=> console.log('server started successfully !!')); //
+//app.listen( process.env.PORT || configs.SERVER_PORT , ()=> console.log('server started successfully !!')); //
 
