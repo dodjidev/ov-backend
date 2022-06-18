b81b0103b5f684c04d4d89fe869abf093d57062f
dadev <dadevcontact@gmail.com>
2022-06-18 07:26:46 +0100

    add lat & lng to user model

--- src/models/user.mdl.js
+++ src/models/user.mdl.js
@@ -14,7 +14,12 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true
-    }
+    },
+    address: {
+        type: String
+    },
+    lat: String,
+    lng: String
 },{timestamps: true})
 
 
